6e8360330ac228269e83062d021472e2a03197f2
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2015-05-17 18:13:51 -0400

    ported app/lib to bower_components

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,9 @@
 'use strict';
 
-angular.module('angularRestfulAuth', [
-    'ngStorage',
-    'ngRoute',
-    'angular-loading-bar'
-])
-.config(['$routeProvider', '$httpProvider', function ($routeProvider, $httpProvider) {
+angular.module('angularRestfulAuth', ['ngStorage', 'ngRoute', 'angular-loading-bar'])
+    .config(['$routeProvider', '$httpProvider', function($routeProvider, $httpProvider) {
 
-    $routeProvider.
+        $routeProvider.
         when('/', {
             templateUrl: 'partials/home.html',
             controller: 'HomeCtrl'
@@ -28,23 +24,23 @@ angular.module('angularRestfulAuth', [
             redirectTo: '/'
         });
 
-    $httpProvider.interceptors.push(['$q', '$location', '$localStorage', function($q, $location, $localStorage) {
-            return {
-                'request': function (config) {
-                    config.headers = config.headers || {};
-                    if ($localStorage.token) {
-                        config.headers.Authorization = 'Bearer ' + $localStorage.token;
+        $httpProvider.interceptors.push(['$q', '$location', '$localStorage',
+            function($q, $location, $localStorage) {
+                return {
+                    'request': function(config) {
+                        config.headers = config.headers || {};
+                        if ($localStorage.token) {
+                            config.headers.Authorization = 'Bearer ' + $localStorage.token;
+                        }
+                        return config;
+                    },
+                    'responseError': function(response) {
+                        if (response.status === 401 || response.status === 403) {
+                            $location.path('/signin');
+                        }
+                        return $q.reject(response);
                     }
-                    return config;
-                },
-                'responseError': function(response) {
-                    if(response.status === 401 || response.status === 403) {
-                        $location.path('/signin');
-                    }
-                    return $q.reject(response);
-                }
-            };
-        }]);
-
-    }
-]);
+                };
+            }
+        ]);
+    }]);
